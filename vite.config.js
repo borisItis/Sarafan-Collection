1a29a3a4f5a3fed2bce63feec9514fa2346c4ca7
Boris Bovkunovich <yakov.antipov.94@mail.ru>
2025-12-11 13:16:55 +0300

    proda

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue()],
   base: '/Sarafan-Collection/',
   resolve: {
     alias: {
